8134b19b3e1057d5b1ca943148b5ca2db3e4b4cb
Bodil Stokke <bodil@bodil.org>
2013-07-18 02:20:17 +0200

    Swipe left or right to reload.

--- static/keys.js
+++ static/keys.js
@@ -1,4 +1,15 @@
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 39)
+(function() {
+  function reload() {
     document.location.pathname = "/";
-}, false);
+  }
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13 || event.keyCode === 32 || event.keyCode === 39)
+      reload();
+  }, false);
+
+  Hammer(document.body)
+    .on("dragleft", reload)
+    .on("dragright", reload);
+
+})();
